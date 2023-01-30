842d7eeeadb10a04c3721fdc15b351d4864e4f1f
Hooshmand <sogandprh@gmail.com>
2023-01-30 23:45:03 +0330

    second commit

--- op.php
+++ op.php
@@ -8,7 +8,19 @@
 
     }
 
-//    class kamion implements bar {
-//        public function setNitro($nitro);
-//
-//    }
+    class kamion implements bar {
+       public function setVazn($vazn)
+       {
+           // TODO: Implement setVazn() method.
+       }
+    }
+
+    class motor implements turbo {
+        public function setNitro($nitro)
+        {
+            // TODO: Implement setNitro() method.
+        }
+    }
+
+    $kavir = new motor();
+    $kavir->setNitro(30);
