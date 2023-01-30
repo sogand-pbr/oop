be6727ba3bcd0e0d5153f08157b738d91f985388
Hooshmand <sogandprh@gmail.com>
2023-01-30 23:40:01 +0330

    first commit

---
+++ op.php
@@ -0,0 +1,14 @@
+<?php
+    interface bar {
+        public function setVazn($vazn);
+
+    }
+    interface turbo{
+        public function setNitro($nitro);
+
+    }
+
+//    class kamion implements bar {
+//        public function setNitro($nitro);
+//
+//    }
